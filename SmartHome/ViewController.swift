585f76a7a3074474afc9986bc2362724496380ee
MacOS <macos@macbook-pro-macos-2.local>
2017-08-31 20:21:15 +0200

    Initial Commit

---
+++ SmartHome/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartHome
+//
+//  Created by MacOS on 31/08/17.
+//  Copyright © 2017 Michał Ryś. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
